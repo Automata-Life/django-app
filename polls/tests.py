802efe7dfbeda9dacf3765831b766c029531cc63
phrb <pedro.bruel@gmail.com>
2016-09-30 14:54:45 -0300

    Add unit test sample.
    @phrb, @rilifon, @carybe, @caio1

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+
+class RootTestCase(TestCase):
+    def test_root_page(self):
+        c = Client()
+        response = c.get('/')
+        print(response.content)
+        self.assertTrue("Hello, is it me you are looking for?" in response.content)
